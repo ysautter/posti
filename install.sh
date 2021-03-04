e6e6112bab64998e348f5b6cbfcd3cd201ba4b46
Yannick <y.sautter@tuxguard.com>
2021-03-04 17:19:34 +0100

    Fixed missing read parameter

--- install.sh
+++ install.sh
@@ -2,15 +2,15 @@
 
 function install () {
     echo -n "$1 [Y|n]"
-    read -s -n install
+    read -s -n 1 install
     echo
     if [ "$install" != "n" ]; then
         bash $2
     fi
 }
 
+install "INSTALL SOFTWARE (PACMAN)" ./software-pacman.sh
 install "INSTALL XORG" ./xorg.sh
 install "INSTALL i3" ./i3.sh
-install "INSTALL SOFTWARE (PACMAN)" ./software-pacman.sh
 install "INSTALL SOFTWARE (MANUAL)" ./software-manual.sh
 install "ADD USER AND DOWNLOAD DOTFILES" ./setup.sh
