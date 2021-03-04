dfd5b60a908021883aa64b7fce457ad632a4117b
Yannick <y.sautter@tuxguard.com>
2021-03-04 16:42:37 +0100

    Added user prompt

--- additional-software.sh
+++ additional-software.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
+sudo -i -u $user bash << EOF
+
 echo
-echo "INSTALLING AUR SOFTWARE"
+echo "INSTALLING AUR SOFTWARE [Y|n]:"
+read -s -n 1 install_aur
+
 echo
 
+if [ "$install_aur" != "n" ]; then
 cd "/tmp"
 
 echo "CLONING: YAY"
-
-
-sudo -i -u $user bash << EOF
 
 git clone https://aur.archlinux.org/yay.git
 cd yay
@@ -23,14 +25,18 @@ echo
 echo "Done!"
 echo
 bash /home/$user/.vim/plugged/YouCompleteMe/install.sh
+fi
 
 echo
-echo "INSTALLING MANUAL SOFTWARE"
+echo "INSTALLING MANUAL SOFTWARE [Y|n]"
+read -s -n 1 install_manual
 echo
+if [ "$install_manual" != "n" ]; then
 echo "INSTALLING LUKE SMITH ST"
 git clone https://github.com/LukeSmithxyz/st
 cd st
 sudo make install
+fi
 
 EOF
 
