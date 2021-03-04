4c18c02e6305ad7ef10e2a939a1eada6bfa2a035
Yannick <y.sautter@tuxguard.com>
2021-03-04 13:32:17 +0100

    created additional-software.sh

--- additional-software.sh
+++ additional-software.sh
@@ -13,17 +13,24 @@ sudo -i -u $user bash << EOF
 
 git clone https://aur.archlinux.org/yay.git
 cd yay
-echo "Y" | makepkg -si
+makepkg -si --noconfirm
 
 yay -S 'picom-tryone-git' --noconfirm --needed
-yay -S 'spotify' --noconfimg --needed
-yay -S 'latex-mk' --noconfimg --needed
+yay -S 'spotify' --noconfirm --needed
+yay -S 'latex-mk' --noconfirm --needed
 
 echo
 echo "Done!"
 echo
 bash /home/$user/.vim/plugged/YouCompleteMe/install.sh
 
+echo
+echo "INSTALLING MANUAL SOFTWARE"
+echo
+echo "INSTALLING LUKE SMITH ST"
+git clone https://github.com/LukeSmithxyz/st
+cd st
+sudo make install
+
 EOF
 
-export user=$user; bash ./manual-software.sh
